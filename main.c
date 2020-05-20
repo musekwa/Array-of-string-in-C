da208bf670512923cf0edf832bc4ce341e3ec99b
Evariste Musekwa Iguna <musekwa2011@gmail.com>
2020-05-20 23:39:49 +0000

    some printf info

--- main.c
+++ main.c
@@ -1,35 +1,50 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void imprimeTodosNomes(char **nomes){
   int i = 0;
-  printf("\nOs Nomes sao...\n");
+   printf("\n===========================================================\n\n");
+  printf("\nTodos os nomes introduzidos sao...\n");
   while (nomes[i] != NULL){
     printf ("%s  ", nomes[i]);
     i++;
+  }
+  if(i == 0){
+       printf("Desculpe, nenhum nome foi introduzido'\n");
   }
 }
 
 void imprimeNomesComAS(char **nomes){
   int i = 0;
-  printf("\nOs Nomes que contem \'as\' sao...\n");
+  int contador = 0;
+   printf("\n===========================================================\n\n");
+  printf("\nOs nomes que contem a sequencia de letras \'as\' sao...\n");
   while (nomes[i] != NULL){
-    printf("Estou dentro\n");
-  
-    if (strlen(nomes[i]) < 2){
-      continue;
-    }
-    char a, s;
-    for(int j = 1; j <= strlen(nomes[i]); j++){
-      if ((nomes[i][j-1] == 'a' || nomes[i][j-1] == 'A' )&& (nomes[i][j] == 's' || nomes[i][j-1] == 'S')){
-        printf(" %s ", nomes[i]);
-        continue;
+    if (strlen(nomes[i]) >= 2){    
+      char a, s;
+      for(int j = 1; j <= strlen(nomes[i]); j++){
+        if ((nomes[i][j-1] == 'a' || nomes[i][j-1] == 'A' )&& (nomes[i][j] == 's' || nomes[i][j] == 'S')){
+          printf("%s  ", nomes[i]);
+          contador++;
+          break;
+        }
       }
     }
-
     i++;
   }
+  if (contador == 0){
+      printf("Desculpe, nenhum nome contem a sequencia de letas \'as\'\n");
+  }
+  printf("\n===========================================================\n\n");
 }
 
 void criaListaDeNomes(){
@@ -40,7 +55,7 @@ void criaListaDeNomes(){
   char **tmp;
   nomes = malloc(sizeof(char*)*size);
   int length = sizeof(nomes);
-   
+  printf("\n===========================================================\n\n");
   while (1){
     nomes[it] = malloc(50*sizeof(char));
     printf("Introduza nome (ou \'fim\' para terminar): ");
@@ -72,3 +87,4 @@ int main(void) {
 
   return 0;
 }
+
